9be3d7e104a5bde1703238593fdef04ffb460dfe
Nikolay Georgiev <3rahmann@gmail.com>
2022-02-06 14:19:54 +0200

    client

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+
+  return (
+    <React.Fragment>
+      <Header />
+      <Posts posts={posts} />
+      <Sidebar />
+    </React.Fragment>
+  );
+}
