e68630bb3ca502f057766c264e42361d9b3fc255
Nikolay Georgiev <3rahmann@gmail.com>
2022-02-06 14:22:06 +0200

    server

--- server/index.js
+++ server/index.js
@@ -36,6 +36,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+console.log("asd");
 
 app.listen("5000", () => {
   console.log("Backend is running.");
